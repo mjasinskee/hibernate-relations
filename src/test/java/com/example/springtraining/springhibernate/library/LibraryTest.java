e9dfe2048deaf20dc915d5155a1db597e193cd40
marcin jasinski <m.jasinskee@gmail.com>
2020-11-08 12:25:52 +0100

    save and update only via bookrepo

--- src/test/java/com/example/springtraining/springhibernate/library/LibraryTest.java
+++ src/test/java/com/example/springtraining/springhibernate/library/LibraryTest.java
@@ -1,12 +1,19 @@
 package com.example.springtraining.springhibernate.library;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class LibraryTest {
 
     @Autowired BookRepository bookRepository;
@@ -27,6 +34,34 @@ class LibraryTest {
         bookRepository.save(book2);
 
         //then
-        System.out.println("book result: " + bookRepository.findAll());
+        List<Book> books = bookRepository.findAll();
+        System.out.println("book result: " + books);
+        assertThat(books.size()).isEqualTo(2);
+//        books.forEach(book -> assertThat());
+//        assertThat()
+        System.out.println("");
+        System.out.println("dupa: " + authorRepository.findAll());
+    }
+
+    @Test
+    public void shouldUpdateAuthor() {
+        //given
+        Author author1 = new Author("name1", "lastName1");
+        Author author2 = new Author("name2", "lastName2");
+        Book book1 = new Book("ISBN1", "title1", author1);
+        Book book2 = new Book("ISBN2", "title2", author2);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+
+        //when
+        Optional<Book> book = bookRepository.findById(8L);
+        book.ifPresent(b -> {
+            b.getAuthor().setFirstName("AnotherName");
+            bookRepository.save(b);
+        });
+
+        //then
+        Optional<Book> saved = bookRepository.findById(8L);
+        assertThat(saved.get().getAuthor().getFirstName()).isEqualToIgnoringCase("anothername");
     }
 }
