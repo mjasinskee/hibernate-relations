4a8df1da3324fc0a519a22795419839104bfc1ae
marcin jasinski <m.jasinskee@gmail.com>
2020-11-08 14:00:19 +0100

    new querry

--- src/main/java/com/example/springtraining/springhibernate/library/BookRepository.java
+++ src/main/java/com/example/springtraining/springhibernate/library/BookRepository.java
@@ -1,6 +1,13 @@
 package com.example.springtraining.springhibernate.library;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("select b  from Book b where b.author = :author")
+    List<Book> findBookByAuthor(@Param("author") Author author);
 }
--- src/test/java/com/example/springtraining/springhibernate/library/LibraryTest.java
+++ src/test/java/com/example/springtraining/springhibernate/library/LibraryTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -66,6 +65,26 @@ class LibraryTest {
         Optional<Book> saved = bookRepository.findById(6L);
         assertThat(saved.isPresent()).isTrue();
         assertThat(saved.get().getAuthor().getFirstName().equalsIgnoreCase("anothername"));
+    }
+
+    @Test
+    public void shouldFindBooksByAuthor() {
+        //given
+        Author author1 = new Author("name1", "lastname1");
+        Author author2 = new Author("name2", "lastname2");
+        Book book1 = new Book("ISBN1", "title1", author1);
+        Book book2 = new Book("ISBN2", "title2", author1);
+        Book book3 = new Book("ISBN3", "title3", author2);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+        bookRepository.save(book3);
+        Author author = authorRepository.findAll().get(0);
+
+        //when
+        List<Book> bookByAuthor = bookRepository.findBookByAuthor(author);
+
+        //then
+        assertThat(bookByAuthor.size()).isEqualTo(2);
     }
 
 }
